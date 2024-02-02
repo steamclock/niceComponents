e1b1ed30d413cf203c6a46683b614f4fa469e116
Nigel Brooke <nigel.brooke@gmail.com>
2024-02-02 10:52:27 -0800

    Generate a full memberwise init

--- Sources/NiceInitMacros/NiceInitMacro.swift
+++ Sources/NiceInitMacros/NiceInitMacro.swift
@@ -17,13 +17,39 @@ public struct NiceInitMacro: MemberMacro {
         providingMembersOf declaration: some DeclGroupSyntax,
         in context: some MacroExpansionContext
     ) throws -> [DeclSyntax] {
-        [
-            """
-            public init(dummy: Bool) {
-                self.init(surfaceColor: .white, onSurfaceColor: .white)
+        let memberList = declaration.memberBlock.members
+
+        let properties = memberList.compactMap { member -> (String, TypeSyntax)? in
+            // is a property
+            guard
+                let binding = member.decl.as(VariableDeclSyntax.self)?.bindings.first,
+                let propertyName = binding.pattern.as(IdentifierPatternSyntax.self)?.identifier.text,
+                let propertyType = binding.typeAnnotation?.type
+            else {
+                return nil
             }
-            """
-        ]
+
+            return (propertyName, propertyType)
+        }
+
+        var baseInit: String = "public init(\n"
+        var first: Bool = true
+        for property in properties {
+            if !first {
+                baseInit += ",\n"
+            } else {
+                first = false
+            }
+            
+            baseInit += "    \(property.0): \(property.1)"
+        }
+        baseInit += "\n) {\n"
+        for property in properties {
+            baseInit += "    self.\(property.0) = \(property.0)"
+        }
+        baseInit += "}"
+
+        return [DeclSyntax(stringLiteral: baseInit)]
     }
 }
 
